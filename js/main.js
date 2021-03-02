2acde73c1b1b139b8b41c724f63fd1496bddec14
josef-alnegren-nti-johanneberg <74712657+josef-alnegren-nti-johanneberg@users.noreply.github.com>
2021-03-02 16:00:24 +0100

    2021-03-02
    tisdag

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ document.querySelector(".drop_button").addEventListener("click", showMenu)
 
 function showMenu() {
     document.querySelector(".drop-down").classList.toggle("show")
+    document.querySelector(".drop_button").classList.toggle("rotate")
 }
