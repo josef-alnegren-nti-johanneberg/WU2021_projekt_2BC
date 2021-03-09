c1664e5658b56187cb090171facfbcc356f3a976
josef-alnegren-nti-johanneberg <74712657+josef-alnegren-nti-johanneberg@users.noreply.github.com>
2021-03-09 20:49:20 +0100

    kvällen
    fixade fade in och snyggare rubriker, finfint

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-document.querySelector(".drop_button").addEventListener("click", showMenu)
+
+function fadeIn() {
+    document.querySelector(".prime_article").classList.toggle("fade")
+}
 
 function showMenu() {
     document.querySelector(".drop-down").classList.toggle("show")
     document.querySelector(".drop_button").classList.toggle("rotate")
 }
+
+
+document.querySelector(".drop_button").addEventListener("click", showMenu)
+window.addEventListener("load", fadeIn)
